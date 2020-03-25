eac10646d39e85ecff49f5effcf3a6cddf8e4b17
N.van.der.Laan@rug.nl <p272402@peregrine.hpc.rug.nl>
2020-03-25 15:52:18 +0100

    test: monkey comment

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {    
-  Data rand(16351); 
+  Data rand(16351);  // monkey
 
   GRBEnv env;  
   env.set(GRB_IntParam_OutputFlag, 0); 
--- smips/algorithms/decomposition/aggregator/cgmip/check_mp_violation.cpp
+++ smips/algorithms/decomposition/aggregator/cgmip/check_mp_violation.cpp
@@ -2,5 +2,11 @@
 
 bool CGMip::check_mp_violation(double tol)
 {
+  if (d_mp.get(GRB_DoubleAttr_ConstrVio) > tol)
+  {
+    cout << d_mp.get(GRB_DoubleAttr_ConstrVio) << '\n';
+    exit(1);
+  }
+
   return (d_mp.get(GRB_DoubleAttr_ConstrVio) <= tol);
 }
