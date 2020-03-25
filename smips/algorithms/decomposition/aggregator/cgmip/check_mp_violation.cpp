d8514a12ed133bf4048ef11d4ea96942d217ea90
N.van.der.Laan@rug.nl <p272402@peregrine.hpc.rug.nl>
2020-03-25 15:53:19 +0100

    test: monkey comment

--- smips/algorithms/decomposition/aggregator/cgmip/check_mp_violation.cpp
+++ smips/algorithms/decomposition/aggregator/cgmip/check_mp_violation.cpp
@@ -2,11 +2,5 @@
 
 bool CGMip::check_mp_violation(double tol)
 {
-  if (d_mp.get(GRB_DoubleAttr_ConstrVio) > tol)
-  {
-    cout << d_mp.get(GRB_DoubleAttr_ConstrVio) << '\n';
-    exit(1);
-  }
-
   return (d_mp.get(GRB_DoubleAttr_ConstrVio) <= tol);
 }
