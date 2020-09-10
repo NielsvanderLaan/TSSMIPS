8ae57611504228acce0e80cd742db39bc0f7eef1
NielsvdL <nielsvanderlaan@hotmail.com>
2020-09-10 09:32:58 +0200

    if sub is difficult, set sub mipgap = 0

--- smips/algorithms/decomposition/aggregator/cgmip/solve_sub.cpp
+++ smips/algorithms/decomposition/aggregator/cgmip/solve_sub.cpp
@@ -10,11 +10,24 @@ CGMip::Point CGMip::solve_sub()
     cout << "sub status: " << status << '\n';
     d_sub.write("sub.lp");
     exit(1);
-  }  
+  }
+
+  double rhs_lb = d_sub.get(GRB_DoubleAttr_ObjBound);
+  double rhs_ub = d_sub.get(GRB_DoubleAttr_ObjVal);
+  if (rhs_lb < rhs_ub - 1e-4)
+  {
+    d_sub.set(GRB_DoubleParam_MIPGap, 0.0);
+    d_sub.optimize();
+    d_sub.set(GRB_DoubleParam_MIPGap, 1e-4);
+    rhs_lb = d_sub.get(GRB_DoubleAttr_ObjBound);
+    rhs_ub = d_sub.get(GRB_DoubleAttr_ObjVal);
+  }
   
   double *xVals = d_sub.get(GRB_DoubleAttr_X, d_xVars.data(), d_xVars.size());
   vector<double> x{ xVals, xVals + d_xVars.size() };
   delete[] xVals;
 
-  return Point{ x, d_theta.get(GRB_DoubleAttr_X), d_eta.get(GRB_DoubleAttr_X), d_sub.get(GRB_DoubleAttr_ObjBound), d_sub.get(GRB_DoubleAttr_ObjVal) };
+
+
+  return Point{ x, d_theta.get(GRB_DoubleAttr_X), d_eta.get(GRB_DoubleAttr_X), rhs_lb, rhs_ub };
 }
