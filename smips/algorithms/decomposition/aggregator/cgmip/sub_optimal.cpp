102eeb4f210c9a01d2c32878ac7d7cf0a1d7cde8
NielsvdL <nielsvanderlaan@hotmail.com>
2020-09-21 16:42:30 +0200

    M = 1e9 (mp coeff cap) no exit if sub_status = 13

--- smips/algorithms/decomposition/aggregator/cgmip/sub_optimal.cpp
+++ smips/algorithms/decomposition/aggregator/cgmip/sub_optimal.cpp
@@ -12,7 +12,7 @@ void CGMip::sub_optimal()
     cout << "sub out of time, gap = " << d_sub.get(GRB_DoubleAttr_ObjVal) - d_sub.get(GRB_DoubleAttr_ObjBound);
     return;
   }
-  if (status != 2)
+  if (status != 2 and status != 13)
   {
     cout << "sub status: " << status << '\n';
     d_sub.write("sub.lp");
