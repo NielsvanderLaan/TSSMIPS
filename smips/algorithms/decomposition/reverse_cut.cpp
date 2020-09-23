742a889d7358a1ddd2b11882ecf8d64a35d26693
NielsvdL <nielsvanderlaan@hotmail.com>
2020-09-23 16:50:11 +0200

    reverse cuts implemented (not for L&P cuts)

--- smips/algorithms/decomposition/reverse_cut.cpp
+++ smips/algorithms/decomposition/reverse_cut.cpp
@@ -4,7 +4,7 @@
 void Benders::reverse_cut(double UB)
 {
   d_master.reverse_cut(UB);
-  //d_agg.reverse_cut(UB);
+  d_agg.reverse_cut(UB);
     // TODO delegate to d_pslp
     // d_pslp in turn delegates to d_zk whose elements delegate to their cglps
 }
