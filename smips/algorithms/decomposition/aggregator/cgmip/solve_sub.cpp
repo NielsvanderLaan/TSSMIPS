1aead8dbafb5ccb81669ae60364c16b0a9a8806f
NielsvdL <nielsvanderlaan@hotmail.com>
2020-10-08 22:13:15 +0200

    undo

--- smips/algorithms/decomposition/aggregator/cgmip/solve_sub.cpp
+++ smips/algorithms/decomposition/aggregator/cgmip/solve_sub.cpp
@@ -4,7 +4,6 @@ CGMip::Point CGMip::solve_sub(bool focus)
 {
   d_sub.set(GRB_DoubleParam_MIPGap, focus ? 0.0 : 1e-4);
 
-  d_sub.write("sub.lp");
   d_sub.optimize();
   double rhs_lb = d_sub.get(GRB_DoubleAttr_ObjBound);
   double rhs_ub;
