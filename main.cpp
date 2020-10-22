31aa5c90b391c8174aa5c6a790d8eaa2e700a4c3
NielsvdL <nielsvanderlaan@hotmail.com>
2020-10-22 16:39:35 +0200

    no callback (comparison)

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 
       DEF smip(problem, env);
       BendersCallback cb(problem, env, c_env, smip);
-      smip.d_model.setCallback(&cb);
+      //smip.d_model.setCallback(&cb);
       smip.solve();
 
 
