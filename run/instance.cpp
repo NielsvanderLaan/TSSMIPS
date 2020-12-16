ebc1b1ea6b2c16210dfe3f3a256e689a59a84051
NielsvdL <nielsvanderlaan@hotmail.com>
2020-12-16 10:44:49 +0100

    multithreading release: no warm start, tol = 1e-6

--- run/instance.cpp
+++ run/instance.cpp
@@ -19,7 +19,7 @@ void instance(Problem &problem, int argc, char *argv[])
   {
     cout << "SSV_" << argv[2] << '_' << argv[3] << '_' << argv[4] << '_' << argv[5] << '\n';
     problem.ssv95(stoi(argv[2]), stoi(argv[3]), stoi(argv[4]), stoi(argv[5]));
-    problem.enforce_ccr(1e4);
+    //problem.enforce_ccr(1e4);
   }
   if (instance == "CAROE")
   {
