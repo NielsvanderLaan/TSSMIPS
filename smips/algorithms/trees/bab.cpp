c53e5cad4da7a8446288ed157af59116e1ef7655
NielsvdL <nielsvanderlaan@hotmail.com>
2021-05-10 22:28:06 +0200

    tree_cap = 8

--- smips/algorithms/trees/bab.cpp
+++ smips/algorithms/trees/bab.cpp
@@ -19,7 +19,7 @@ vector<double> Tree::bab(vector<Type> types, bool rcuts, bool fenchel, int max_r
     size_t node_idx = distance(d_LB_nodes.begin(), min_element(d_LB_nodes.begin(), d_LB_nodes.end()));
     cout << "\nExploring node " << node_idx << endl;
       // determine max_rounds based on tree_size
-    size_t tree_cap = 100;
+    size_t tree_cap = 8;
     bool branch = solve(types, node_idx, incumbent, tol, time_limit - elapsed, rcuts, fenchel, tree_size <= tree_cap ? max_rounds : 0);  // solve() also updates global bounds
     cout << "GLOBAL LB = " << d_LB_global << " GLOBAL UB = " << d_UB_global << endl;
     
